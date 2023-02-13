7d7b8bf7b446f748acb48ee1defdc02b208252d9
Austin <austin_farris1@baylor.edu>
2023-02-13 13:38:41 -0600

    printed old and new salary

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -11,20 +11,24 @@ Iternate through the dictionary to print out their name and thier new salary (as
 import csv
 
 #open the file
+infile = open("employee_data.csv", 'r')
 
+reader = csv.reader(infile)
 
-
+next(reader)
 
 #create an empty dictionary
-
+nsDict = {}
 
 #use a loop to iterate through the csv file
-
+for row in reader:
 
     #check if the employee fits the search criteria
-
-
-    
+    if row[3] == 'Marketing' and row[4] == "CSR":
+        print("Manager Name: ", row[1],' ', row[2], ' Current Salary: $', format(float(row[5]), ',.2f'), sep='')
+        key = row[1] + ' ' + row[2]
+        new_sal = (int(row[5]) * .1) + int(row[5])
+        nsDict[key] = new_sal 
 
 print()
 print('=========================================')
@@ -33,9 +37,8 @@ print()
 #iternate through the dictionary and print out the key and value as per printout
 
 
-
-          
-        
+for i in nsDict:
+    print("Manager Name: ", i, " New Salary: $", format(nsDict[i], ',.2f'), sep='')
 
         
     
